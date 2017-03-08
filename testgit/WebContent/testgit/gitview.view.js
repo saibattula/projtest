c099e0d97af30e52889236b46715c9c52d8e9df7
saisankararao.b <saisankararao.b@USABOS157845L.ad.infosys.com>
2017-03-08 00:22:49 -0500

    initial

---
+++ testgit/WebContent/testgit/gitview.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("testgit.gitview", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf testgit.gitview
+	*/ 
+	getControllerName : function() {
+		return "testgit.gitview";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf testgit.gitview
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Git Page",
+			content: [
+			
+			]
+		});
+	}
+
+});
